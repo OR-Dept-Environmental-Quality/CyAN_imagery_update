deac8942c2c58ddf4bd30ff03e663a532a6b1d4d
Yuan Grund <yuan.grund@state.or.us>
2021-08-23 09:54:22 -0700

    data updated till 8/22/2021

--- data.R
+++ data.R
@@ -75,7 +75,8 @@ Carpenter, Kurt <kdcar@usgs.gov>;
 Brian Fulfrost <bfaconsult@gmail.com>;
 Lundell, Tina M CIV USARMY CENWP (USA) <Tina.M.Lundell@usace.army.mil>;
 Buccola, Norman L (Norm) CIV USARMY CENWP (USA) <Norman.L.Buccola@usace.army.mil>;
-Bellringer, Holly H CIV USARMY CENWP (USA) <Holly.H.Bellringer@usace.army.mil>"
+Bellringer, Holly H CIV USARMY CENWP (USA) <Holly.H.Bellringer@usace.army.mil>;
+Anderson, Chauncey W <chauncey@usgs.gov>"
 
 email.address_ER <- "BUTCHER Don * DEQ <don.butcher@deq.state.or.us>; 
 DADOLY John * DEQ <john.dadoly@deq.state.or.us>; 
