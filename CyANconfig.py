9ee02aadf2865ea8d2ebc3ab85cd93894c15b519
Yuan Grund <yuan.grund@state.or.us>
2023-01-24 09:34:58 -0800

    Update CyANconfig.py

--- CyANconfig.py
+++ CyANconfig.py
@@ -4,12 +4,9 @@ baseurl = "https://oceancolor.gsfc.nasa.gov/CYAN/OLCI/"
 # tif_file_path need to be updated for the year folder:
 tif_file_path = '\\\\deqhq1\\wq-share\\Harmful Algal Blooms Coordination Team\\HAB_Shiny_app\\data\\2023\\*.tif'
 
-# extract_base = '\\\\deqhq1\\wq-share\\Harmful Algal Blooms Coordination Team\\GIS\\cyan\\'
 extract_base = '\\\\deqhq1\\wq-share\\Harmful Algal Blooms Coordination Team\\Satellite data\\cyan\\'
 zones = r"\\deqhq1\WQ-Share\Harmful Algal Blooms Coordination Team\HAB_Shiny_app\data\CyAN_Waterbodies.shp"
 dir_Shiny = "\\\\deqhq1\\wq-share\\Harmful Algal Blooms Coordination Team\\HAB_Shiny_app\\data"
-# themask = r"\\deqhq1\wq-share\Harmful Algal Blooms Coordination Team\GIS\cyan\HAB_deschutes2020.gdb\stateline_buffer50albers"
-# theextent = r"\\deqhq1\wq-share\Harmful Algal Blooms Coordination Team\GIS\cyan\HAB_deschutes2020.gdb\stateline_buffer50web"
 themask = r"\\deqhq1\wq-share\Harmful Algal Blooms Coordination Team\Satellite data\cyan\HAB_deschutes2020.gdb\stateline_buffer50albers"
 theextent = r"\\deqhq1\wq-share\Harmful Algal Blooms Coordination Team\Satellite data\cyan\HAB_deschutes2020.gdb\stateline_buffer50web"
 
